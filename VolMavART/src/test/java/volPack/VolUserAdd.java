afa42915b182d73fd6efc5a0baae5e144c5eae3e
vtjessie <32237821+vtjessie@users.noreply.github.com>
2018-12-17 22:46:18 -0500

    new user surefire error

---
+++ VolMavART/src/test/java/volPack/VolUserAdd.java
@@ -0,0 +1,34 @@
+package volPack;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+
+public class VolUserAdd {
+	public static WebDriver driver;
+
+  @Test
+  public void userAdd() throws InterruptedException {
+	  System.out.println("Ave... Maria");
+	  driver.findElement(By.id("email")).clear();
+	  driver.findElement(By.id("password")).clear();
+	  driver.findElement(By.id("email")).sendKeys("vt.jessie@gmail.com");
+	  driver.findElement(By.id("password")).clear();
+	  driver.findElement(By.id("password")).sendKeys("Divine123");
+	  driver.findElement(By.className("login100-form-btn")).click();
+	  Thread.sleep(3000);
+	  driver.findElement(By.id("infotab")).click();
+  }
+  @BeforeTest
+  public void beforeTest() {
+		driver = Util.setgecodriver();
+		driver.get(Util.BASE_URL);
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
